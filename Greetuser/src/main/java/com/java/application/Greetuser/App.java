e4fd6993535422649c8906d89c417148ff710767
iswariyamariyappan <iswariya@smartpricedeal.com>
2023-03-16 21:17:14 +0530

    I created My first commit

---
+++ Greetuser/src/main/java/com/java/application/Greetuser/App.java
@@ -0,0 +1,13 @@
+package com.java.application.Greetuser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
